3e69fa991e12786d704b7e644a73612c7dc8553c
Thanh Vu <thanhvd89@gmail.com>
2021-05-25 04:46:26 +0700

    Improve login form

--- imports/ui/LoginForm.jsx
+++ imports/ui/LoginForm.jsx
@@ -13,27 +13,29 @@ export const LoginForm = () => {
 
   return (
     <form onSubmit={submit} className="login-form">
-      <label htmlFor="username">Username</label>
-
-      <input
-        type="text"
-        placeholder="Username"
-        name="username"
-        required
-        onChange={(e) => setUsername(e.target.value)}
-      />
-
-      <label htmlFor="password">Password</label>
-
-      <input
-        type="password"
-        placeholder="Password"
-        name="password"
-        required
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button type="submit">Log In</button>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <button type="submit">Log In</button>
+      </div>
     </form>
   );
 };
