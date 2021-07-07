138b2460ca7d5b5c30ecb805ff1b525b796b380b
Thanh Vu <thanhvd@phenikaa.ai>
2021-07-07 17:44:29 +0700

    Add react-router-dom

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import React, { useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { useTracker } from "meteor/react-meteor-data";
 import { Task } from "./Task";
 import { TasksCollection } from "/imports/db/TasksCollection";
@@ -53,6 +54,37 @@ export const App = () => {
   return (
     <>
       <CssBaseline />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       <div className="app">
         <header>
           <div className="app-bar">
@@ -101,3 +133,15 @@ export const App = () => {
     </>
   );
 };
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
