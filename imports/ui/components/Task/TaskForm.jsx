8b22fdedab9d5cc43f2097bd9ce05048073ce64c
Thanh Vu <thanhvd89@gmail.com>
2021-07-11 02:07:23 +0700

    Apply react hook form to Task form

--- imports/ui/components/Task/TaskForm.jsx
+++ imports/ui/components/Task/TaskForm.jsx
@@ -1,29 +1,52 @@
-import React, { useState } from "react";
+import { Meteor } from "meteor/meteor";
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 
+const schema = yup.object().shape({
+  text: yup.string().required().label("Task Name"),
+});
+
 export const TaskForm = () => {
-  const [text, setText] = useState("");
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      text: "",
+    },
+    resolver: yupResolver(schema),
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!text) return;
-
-    Meteor.call("tasks.insert", text);
-
-    setText("");
+  const onSubmit = ({ text }) => {
+    Meteor.call("tasks.insert", text, (error) => {
+      if (!error) {
+        reset();
+      }
+    });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Box mb={2}>
-        <TextField
-          label="Type to add new tasks"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          fullWidth
+        <Controller
+          name="text"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Task Name"
+              fullWidth
+              error={Boolean(errors.text?.message)}
+              helperText={errors.text?.message}
+            />
+          )}
         />
       </Box>
 
